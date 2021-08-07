ef4b41100720cd1372b2727feb442f337e2d8781
Diego Mello <diegofemello@gmail.com>
2021-08-07 12:28:26 -0300

    apagado .vs

---
+++ ProEventos.API/Controllers/EventosController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProEventos.Domain;
+using ProEventos.Repository;
+using ProEventos.Repository.Contexts;
+using ProEventos.Sevice.Interfaces;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventosController : ControllerBase
+    {
+        private readonly IEventoService _eventoService;
+
+        public EventosController(IEventoService eventoService)
+        {
+            _eventoService = eventoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Evento>> Get()
+        {
+            try
+            {
+                var eventos = await _eventoService.GetAllEventosAsync(true);
+                if (eventos == null) return NotFound("Nenhum evento encontrado");
+                return Ok(eventos);
+            }
+            catch (Exception ex)
+            {
+
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Evento>> GetById(int id)
+        {
+            try
+            {
+                var evento = await _eventoService.GetEventoByIdAsync(id, true);
+                if (evento == null) return NotFound("Nenhum evento encontrado");
+                return Ok(evento);
+            }
+            catch (Exception ex)
+            {
+
+                return this.StatusCode(StatusCodes.Status500InternalServerError,
+                    $"Erro ao tentar recuperar eventos. Erro: {ex.Message}");
+            }
+        }
+    }
+}
